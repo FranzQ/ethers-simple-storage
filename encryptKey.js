6a0a16e3d730e50639627312a8fa60a0d29864fe
Franz <franzquarshie@gmail.com>
2022-08-17 21:34:06 +0000

    PK removed

--- encryptKey.js
+++ encryptKey.js
@@ -1,8 +1,8 @@
 const ethers = require("ethers")
 const fs = require("fs-extra")
 
-const pKey = '3c2de746c246bb43bb72850dd5164346e6f94f95a3ea0450ee39c690c569185e'
-const pass = '12345'
+const pKey = ''
+const pass = ''
 
 async function main() {
     const wallet = new ethers.Wallet('')
